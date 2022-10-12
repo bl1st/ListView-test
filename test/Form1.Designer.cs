95f576ae58640e16128e73d1cf72ed1acadbcd7f
bl1st <balaninben222@gmail.com>
2022-10-12 13:10:33 +0300

    Добавьте файлы проекта.

---
+++ test/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lv = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lv
+            // 
+            this.lv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lv.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.lv.FullRowSelect = true;
+            this.lv.HideSelection = false;
+            this.lv.Location = new System.Drawing.Point(12, 50);
+            this.lv.Name = "lv";
+            this.lv.Size = new System.Drawing.Size(743, 322);
+            this.lv.TabIndex = 0;
+            this.lv.UseCompatibleStateImageBehavior = false;
+            this.lv.View = System.Windows.Forms.View.Details;
+            this.lv.SizeChanged += new System.EventHandler(this.lv_SizeChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "";
+            this.columnHeader1.Width = 500;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 375);
+            this.Controls.Add(this.lv);
+            this.Name = "Form1";
+            this.Text = "A";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lv;
+        protected System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ test/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            Random rand = new Random();
+            ImageList img = new ImageList();
+       
+            
+            img.ImageSize = new Size(90, 90);
+            for (int i =0; i < 5; i++)
+            {              
+                img.Images.Add(new Bitmap(Properties.Resources._1));
+            }
+
+            lv.SmallImageList = img;
+
+            for (int i = 0; i < 5; i++)
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.ImageIndex = i;
+                lvi.Text = "Some text " + rand.Next(100)+ "\nCost : 5000 \nManufacturer: SPBKIT";             
+                lv.Items.Add(lvi);
+            }
+        }
+
+        private void lv_SizeChanged(object sender, EventArgs e)
+        {
+            lv.Columns[0].Width = lv.Width - 4 - SystemInformation.VerticalScrollBarWidth;
+        }
+
+
+    }
+}
